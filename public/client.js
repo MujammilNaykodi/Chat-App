9f822d81e6abfe96b4c5543ddfb45784ccac76aa
MUJAMMIL NAYKODI <119591148+MujammilNaykodi@users.noreply.github.com>
2023-12-17 20:40:31 +0530

    Add files via upload

---
+++ public/client.js
@@ -0,0 +1,55 @@
+
+const socket = io()
+let name;
+let textarea = document.querySelector('#textarea')//for sending message when enter key is pressed
+let messageArea = document.querySelector('.message__area')
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)///e--> is an event object and target.value is DOM property //e.target: This represents the DOM element that triggered the event. In the context of the event listener for the keyup event on the textarea, e.target would be the textarea element itself.
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+
+ 
